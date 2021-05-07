efe53c6d01e6d34b6b5c18cec58ac0b35a9c0c59
Stefaan Lippens <stefaan.lippens@vito.be>
2021-05-07 16:30:38 +0200

    initial tests for MultiBackendConnection

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,6 +1,9 @@
-from openeo_aggregator.backend import AggregatorCollectionCatalog, MultiBackendConnection
-
 import pytest
+
+from openeo import Connection
+from openeo_aggregator.backend import AggregatorCollectionCatalog, MultiBackendConnection, BackendConnection
+
+import types
 
 
 @pytest.fixture
@@ -11,6 +14,26 @@ def multi_backend_connection(requests_mock) -> MultiBackendConnection:
         "oeo1": "https://oeo1.test/",
         "oeo2": "https://oeo2.test/",
     })
+
+
+class TestMultiBackendConnection:
+
+    # TODO test version discovery in constructor
+
+    def test_iter(self, multi_backend_connection):
+        count = 0
+        for x in multi_backend_connection:
+            assert isinstance(x, BackendConnection)
+            assert isinstance(x.connection, Connection)
+            count += 1
+        assert count == 2
+
+    def test_map(self, multi_backend_connection, requests_mock):
+        requests_mock.get("https://oeo1.test/foo", json={"bar": 1})
+        requests_mock.get("https://oeo2.test/foo", json={"meh": 2})
+        res = multi_backend_connection.map(lambda connection: connection.get("foo").json())
+        assert isinstance(res, types.GeneratorType)
+        assert list(res) == [("oeo1", {"bar": 1}), ("oeo2", {"meh": 2})]
 
 
 class TestAggregatorCollectionCatalog:
@@ -28,3 +51,5 @@ class TestAggregatorCollectionCatalog:
         catalog = AggregatorCollectionCatalog(backends=multi_backend_connection)
         metadata = catalog.get_collection_metadata("S2")
         assert metadata == {"id": "S2", "title": "oeo2's S2"}
+
+    # TODO tests for caching of collection metadata
