3dadfe45c3fda109429ee338f8680a1d239063c3
Stefaan Lippens <stefaan.lippens@vito.be>
2021-06-09 09:03:21 +0200

    Add more logging during app building

--- src/openeo_aggregator/app.py
+++ src/openeo_aggregator/app.py
@@ -11,13 +11,15 @@ from openeo_aggregator.backend import AggregatorBackendImplementation, MultiBack
 from openeo_aggregator.config import AggregatorConfig, DEFAULT_CONFIG
 from openeo_driver.server import build_backend_deploy_metadata, setup_logging
 
+_log = logging.getLogger(__name__)
+
 
 def create_app(config: AggregatorConfig = DEFAULT_CONFIG) -> flask.Flask:
     """
     Flask application factory function.
     """
     # This `create_app` factory is auto-detected by Flask's application discovery when running `flask run`
-    # see https://flask.palletsprojects.com/en/2.0.x/cli/#application-discovery)
+    # see https://flask.palletsprojects.com/en/2.0.x/cli/#application-discovery
 
     if config.auto_logging_setup:
         setup_logging(
@@ -25,8 +27,13 @@ def create_app(config: AggregatorConfig = DEFAULT_CONFIG) -> flask.Flask:
             show_loggers=["openeo_driver", "openeo_aggregator"]
         )
 
+    _log.info(f"Creating MultiBackendConnection with {config.aggregator_backends}")
     backends = MultiBackendConnection(backends=config.aggregator_backends)
+
+    _log.info("Creating AggregatorBackendImplementation")
     backend_implementation = AggregatorBackendImplementation(backends=backends, config=config)
+
+    _log.info("Building Flask app")
     app = openeo_driver.views.build_app(
         backend_implementation=backend_implementation,
         error_handling=config.flask_error_handling,
@@ -35,13 +42,14 @@ def create_app(config: AggregatorConfig = DEFAULT_CONFIG) -> flask.Flask:
     deploy_metadata = build_backend_deploy_metadata(
         packages=["openeo", "openeo_driver", "openeo_aggregator"],
     )
-
     app.config.from_mapping(
         OPENEO_TITLE="openEO Aggregator Driver",
         OPENEO_DESCRIPTION="openEO Aggregator Driver",
         OPENEO_BACKEND_VERSION=openeo_aggregator.about.__version__,
         OPENEO_BACKEND_DEPLOY_METADATA=deploy_metadata,
     )
+
+    _log.info(f"Built app {app}")
     return app
 
 
