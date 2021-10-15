f718e0b5ec827dd94abc70fa204f5a5b1bfbc3aa
Stefaan Lippens <stefaan.lippens@vito.be>
2021-10-15 09:55:59 +0200

    Add user name (best effort) to `/me`
    openEOPlatform/architecture-docs#106

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     install_requires=[
         "requests",
         "openeo>=0.8.3a3.*",
-        "openeo_driver>=0.14.5a1.*",
+        "openeo_driver>=0.14.6a1.*",
         "flask~=2.0",
         "gunicorn~=20.0",
     ],
