8b31312d032ab65f175e3d56d1b1949ef3286888
Stefaan Lippens <stefaan.lippens@vito.be>
2022-01-10 10:50:18 +0100

    Enable Creodias backend on dev instance

--- conf/aggregator.prod.py
+++ conf/aggregator.prod.py
@@ -34,7 +34,6 @@ config = AggregatorConfig(
     config_source=__file__,
     aggregator_backends={
         "vito": "https://openeo.vito.be/openeo/1.0/",
-        # "creo": "https://openeo.creo.vito.be/openeo/1.0/",
         "eodc": "https://openeo.eodc.eu/v1.0/",
     },
     auth_entitlement_check={"oidc_issuer_whitelist": {"https://aai.egi.eu/oidc"}},
