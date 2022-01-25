f4057517724652cf1c1a755b5c55808bf497c576
Stefaan Lippens <stefaan.lippens@vito.be>
2022-01-25 16:47:48 +0100

    EP-3475 prepare for new job splitting approaches

--- src/openeo_aggregator/partitionedjobs/splitting.py
+++ src/openeo_aggregator/partitionedjobs/splitting.py
@@ -1,25 +1,35 @@
-from openeo_aggregator.connection import MultiBackendConnection
+import abc
+import typing
 from openeo_aggregator.partitionedjobs import PartitionedJob, SubJob
 
+if typing.TYPE_CHECKING:
+    from openeo_aggregator.backend import AggregatorProcessing
 
-class JobSplitter:
+
+class AbstractJobSplitter(metaclass=abc.ABCMeta):
     """
     Split a given "large" batch job in "sub" batch jobs according to a certain strategy,
     for example: split in spatial sub-extents, split temporally, split per input collection, ...
     """
 
-    def __init__(self, backends: MultiBackendConnection):
-        self._backends = backends
+    def __init__(self, processing: "AggregatorProcessing"):
+        self.processing = processing
 
+    @abc.abstractmethod
     def split(self, process: dict, metadata: dict = None, job_options: dict = None) -> PartitionedJob:
         # TODO: how to express dependencies? give SubJobs an id for referencing?
         # TODO: how to express combination/aggregation of multiple subjob results as a final result?
-        return PartitionedJob(
-            process=process,
-            metadata=metadata, job_options=job_options,
-            subjobs=[
-                # TODO: real splitting
-                SubJob(process_graph=process["process_graph"], backend_id=self._backends.first().id),
-            ]
-        )
+        ...
 
+
+class NoSplitter(AbstractJobSplitter):
+    """The simple job splitter that does not split."""
+
+    def split(self, process: dict, metadata: dict = None, job_options: dict = None) -> PartitionedJob:
+        backend_id = self.processing.get_backend_for_process_graph(
+            process_graph=process["process_graph"], api_version="TODO"
+        )
+        subjobs = [
+            SubJob(process_graph=process["process_graph"], backend_id=backend_id),
+        ]
+        return PartitionedJob(process=process, metadata=metadata, job_options=job_options, subjobs=subjobs)
