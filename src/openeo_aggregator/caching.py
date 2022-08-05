4a5b17a150e368e851902617c43e94c6f1c3b1d8
Stefaan Lippens <stefaan.lippens@vito.be>
2022-08-05 17:58:25 +0200

    Issue #2: Initial implementation of ZooKeeper based memoization

--- src/openeo_aggregator/caching.py
+++ src/openeo_aggregator/caching.py
@@ -1,11 +1,22 @@
+import contextlib
+import json
 import logging
 import time
-from typing import Callable
+from typing import Callable, Tuple, Union
+
+import kazoo.exceptions
+import kazoo.protocol.paths
+from kazoo.client import KazooClient
 
 from openeo.util import TimingLogger
+from openeo_aggregator.utils import strip_join, Clock
 
 
 class CacheMissException(KeyError):
+    pass
+
+
+class CacheExpiredException(CacheMissException):
     pass
 
 
@@ -76,3 +87,110 @@ class TtlCache:
 
     def flush_all(self):
         self._cache = {}
+
+
+class ZkMemoizer:
+    """
+    ZooKeeper based caching of function call results.
+
+    Basic usage:
+
+        >>> zk_cache = ZkMemoizer(zk_client, prefix="/myapp/cache/mysubsystem")
+        >>> count = zk_cache.get_or_call("count", callback=calculate_count)
+
+    """
+
+    def __init__(self, client: KazooClient, prefix: str, default_ttl: int = 60, name: str = None):
+        self._client = client
+        self._prefix = prefix
+        self._default_ttl = default_ttl
+        self._name = name or self.__class__.__name__
+
+    def get_or_call(
+            self,
+            key: Union[str, Tuple],
+            callback: Callable,
+            ttl=None,
+            log_on_miss=False,
+    ):
+        """
+        Try to get data from cache or calculate otherwise
+        """
+
+        path = self._path(key)
+        if log_on_miss:
+            timing_logger = TimingLogger(
+                title=f"Cache miss {self._name!r} key {key!r}: calling {callback.__qualname__!r}",
+                logger=_log.debug
+            )
+            # Use decorator functionality of TimingLogger to wrap the callback
+            callback = timing_logger(callback)
+
+        try:
+            with self._connect():
+                try:
+                    zk_value, zk_stat = self._client.get(path=path)
+                    value = self._deserialize(zk_value)
+                    # Expiry logic: note that we evaluate the TTL at "get time",
+                    # instead of storing an expiry threshold at "set time" along with the data
+                    expiry = zk_stat.last_modified + (ttl or self._default_ttl)
+                    now = Clock.time()
+                    if expiry <= now:
+                        _log.debug(f"Cache {self._name!r} key {key!r} expired ({expiry} < {now})")
+                        raise CacheExpiredException
+
+                except kazoo.exceptions.NoNodeError:
+                    # Simple cache miss: no zookeeper node, so we'll create it.
+                    value = callback()
+                    try:
+                        self._client.create(path=path, value=self._serialize(value), makepath=True)
+                    except kazoo.exceptions.NodeExistsError:
+                        # Node has been set in the meantime by other worker probably
+                        _log.warning(f"Tried to create new zk cache node {path!r}, but it already exists.")
+                    except Exception as e:
+                        _log.error(f"Failed to create new zk cache node {path!r}: {e!r}")
+                        # Continue without setting cache
+                except (CacheExpiredException, json.JSONDecodeError):
+                    # Cache hit, but it expired or is invalid: update it
+                    value = callback()
+                    try:
+                        self._client.set(path=path, value=self._serialize(value))
+                    except Exception as e:
+                        _log.error(f"Failed to update zk cache node {path!r}: {e!r}")
+                        # Continue without setting cache
+        except Exception as e:
+            _log.error(f"Failed to get zk cache node {path!r}: {e!r}")
+            value = callback()
+            # TODO: still try to create/set cache in some cases?
+
+        return value
+
+    def _path(self, key: Union[str, Tuple]) -> str:
+        """Helper to build a zookeeper path"""
+        if isinstance(key, str):
+            key = (key,)
+        path = strip_join("/", "", self._prefix, *key)
+        return kazoo.protocol.paths.normpath(path)
+
+    @contextlib.contextmanager
+    def _connect(self):
+        """
+        Context manager to automatically start and stop zookeeper connection.
+        """
+        self._client.start(timeout=5)
+        try:
+            yield self._client
+        finally:
+            self._client.stop()
+
+    @staticmethod
+    def _serialize(value: dict) -> bytes:
+        """Serialize a dictionary (given as arguments) in JSON (UTF8 byte-encoded)."""
+        # TODO: support other serialization (pickle, json+gzip, ...)?
+        # TODO: JSON serialization converts tuples to lists: is that a problem somewhere?
+        return json.dumps(value, indent=None, separators=(',', ':')).encode("utf8")
+
+    @staticmethod
+    def _deserialize(zk_value: bytes) -> dict:
+        """Deserialize bytes (assuming UTF8 encoded JSON mapping)"""
+        return json.loads(zk_value.decode("utf8"))
--- tests/test_caching.py
+++ tests/test_caching.py
@@ -1,8 +1,15 @@
+import collections
+import itertools
 import logging
+from typing import Callable
+from unittest import mock
 
+import kazoo.exceptions
 import pytest
 
-from openeo_aggregator.caching import TtlCache, CacheMissException
+from openeo_aggregator.caching import TtlCache, CacheMissException, ZkMemoizer
+from openeo_aggregator.testing import clock_mock
+from openeo_aggregator.utils import Clock
 
 
 class FakeClock:
@@ -98,3 +105,149 @@ class TestTtlCache:
         caplog.clear()
         assert cache.get_or_call("foo", callback, log_on_miss=True) == 333
         assert caplog.text == ""
+
+
+class TestZkMemoizer:
+
+    def _build_callback(self, start=100) -> Callable[[], int]:
+        """Build callback that just returns increasing numbers on each invocation"""
+        return itertools.count(start=start).__next__
+
+    @pytest.fixture
+    def zk_client(self) -> mock.Mock:
+        """Simple ad-hoc ZooKeeper client fixture using a dictionary for storage."""
+        zk_client = mock.Mock()
+        db = {}
+
+        ZnodeStat = collections.namedtuple("ZnodeStat", ["last_modified"])
+
+        def get(path):
+            if path not in db:
+                raise kazoo.exceptions.NoNodeError
+            return db[path]
+
+        def create(path, value, makepath=False):
+            if path in db:
+                raise kazoo.exceptions.NodeExistsError
+            assert isinstance(value, bytes)
+            db[path] = (value, ZnodeStat(last_modified=Clock.time()))
+
+        def set(path, value):
+            if path not in db:
+                raise kazoo.exceptions.NoNodeError
+            assert isinstance(value, bytes)
+            db[path] = (value, ZnodeStat(last_modified=Clock.time()))
+
+        zk_client.get.side_effect = get
+        zk_client.create.side_effect = create
+        zk_client.set.side_effect = set
+        return zk_client
+
+    def test_basic(self, zk_client):
+        zk_cache = ZkMemoizer(client=zk_client, prefix="test")
+        callback = self._build_callback()
+        zk_client.get.assert_not_called()
+        assert zk_cache.get_or_call(key="count", callback=callback) == 100
+        zk_client.get.assert_called_once_with(path='/test/count')
+
+    def test_broken_client(self):
+        """Test that callback keeps working if ZooKeeper client is broken"""
+        # Create useless client
+        zk_client = object()
+        zk_cache = ZkMemoizer(client=zk_client, prefix="test")
+
+        callback = self._build_callback()
+        assert zk_cache.get_or_call(key="count", callback=callback) == 100
+        assert zk_cache.get_or_call(key="count", callback=callback) == 101
+        assert zk_cache.get_or_call(key="count", callback=callback) == 102
+
+    def test_basic_caching(self, zk_client):
+        zk_cache = ZkMemoizer(client=zk_client, prefix="test")
+        callback = self._build_callback()
+        zk_client.get.assert_not_called()
+        assert zk_cache.get_or_call(key="count", callback=callback) == 100
+        assert zk_cache.get_or_call(key="count", callback=callback) == 100
+        assert zk_cache.get_or_call(key="count", callback=callback) == 100
+
+    def test_caching_expiry(self, zk_client):
+        zk_cache = ZkMemoizer(client=zk_client, prefix="test", default_ttl=1000)
+        callback = self._build_callback()
+        zk_client.get.assert_not_called()
+        with clock_mock(1000):
+            assert zk_cache.get_or_call(key="count", callback=callback) == 100
+        with clock_mock(1999):
+            assert zk_cache.get_or_call(key="count", callback=callback) == 100
+        with clock_mock(2000):
+            assert zk_cache.get_or_call(key="count", callback=callback) == 101
+        with clock_mock(3333):
+            assert zk_cache.get_or_call(key="count", callback=callback) == 102
+        with clock_mock(3500):
+            assert zk_cache.get_or_call(key="count", callback=callback) == 102
+            assert zk_cache.get_or_call(key="count", callback=callback, ttl=60) == 103
+
+    @pytest.mark.parametrize(["prefix", "key", "path"], [
+        ("test", "count", "/test/count"),
+        ("/test/", "/count/", "/test/count"),
+        ("test/v1", "user/count/", "/test/v1/user/count"),
+        ("test", ("count",), "/test/count"),
+        ("test", ("user", "count"), "/test/user/count"),
+        ("test", ("v1", "user", "count", "today"), "/test/v1/user/count/today"),
+        ("test", ["v1", "user", "count", "today"], "/test/v1/user/count/today"),
+    ])
+    def test_key_to_path(self, zk_client, prefix, key, path):
+        zk_cache = ZkMemoizer(client=zk_client, prefix=prefix)
+        callback = self._build_callback()
+        assert zk_cache.get_or_call(key=key, callback=callback) == 100
+        zk_client.get.assert_called_once_with(path=path)
+        # TODO test for having non-strings parts in key?
+
+    def test_caching_create_vs_set(self, zk_client):
+        zk_cache = ZkMemoizer(client=zk_client, prefix="test", default_ttl=100)
+        callback = self._build_callback()
+        assert (0, 0) == (zk_client.create.call_count, zk_client.set.call_count)
+        with clock_mock(1000):
+            assert zk_cache.get_or_call(key="count", callback=callback) == 100
+        assert (1, 0) == (zk_client.create.call_count, zk_client.set.call_count)
+        with clock_mock(2000):
+            assert zk_cache.get_or_call(key="count", callback=callback) == 101
+        assert (1, 1) == (zk_client.create.call_count, zk_client.set.call_count)
+        with clock_mock(3000):
+            assert zk_cache.get_or_call(key="count", callback=callback) == 102
+        assert (1, 2) == (zk_client.create.call_count, zk_client.set.call_count)
+
+    @pytest.mark.parametrize(["data", "expected_prefix"], [
+        (1234, b"1234"),
+        ("just a string", b'"just'),
+        ({"color": "green", "sizes": [1, 2, 3]}, b'{"color":"green'),
+        ([{"id": 3}, {"id": 99}], b'[{"id":'),
+        ([123, None, False, True, "foo"], b'[123'),
+    ])
+    def test_serializing(self, zk_client, data, expected_prefix):
+        zk_cache = ZkMemoizer(client=zk_client, prefix="test")
+
+        callback = mock.Mock(return_value=data)
+
+        assert callback.call_count == 0
+        assert zk_cache.get_or_call(key="count", callback=callback) == data
+        assert callback.call_count == 1
+        assert zk_cache.get_or_call(key="count", callback=callback) == data
+        assert callback.call_count == 1
+
+        zk_client.create.assert_called_once()
+        zk_value = zk_client.create.call_args[1]["value"]
+        assert isinstance(zk_value, bytes)
+        assert zk_value.startswith(expected_prefix)
+
+    def test_corrupted_cache(self, zk_client):
+        zk_cache = ZkMemoizer(client=zk_client, prefix="test", default_ttl=100)
+        callback = self._build_callback()
+        assert zk_cache.get_or_call(key="count", callback=callback) == 100
+        assert zk_cache.get_or_call(key="count", callback=callback) == 100
+
+        assert zk_client.get("/test/count")[0] == b"100"
+        zk_client.set("/test/count", value=b"(orrup[ JS0N d@ta !*")
+        assert zk_client.get("/test/count")[0] == b"(orrup[ JS0N d@ta !*"
+        
+        assert zk_cache.get_or_call(key="count", callback=callback) == 101
+        assert zk_cache.get_or_call(key="count", callback=callback) == 101
+        assert zk_client.get("/test/count")[0] == b"101"
