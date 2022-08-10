d1fb154aa98f29a802e453b7be0c480b012cbf2f
Stefaan Lippens <stefaan.lippens@vito.be>
2022-08-10 15:13:59 +0200

    Use EODC dev instance in aggregator dev config

--- conf/aggregator.dev.py
+++ conf/aggregator.dev.py
@@ -45,7 +45,7 @@ config = AggregatorConfig(
     config_source=__file__,
     aggregator_backends={
         "vito": "https://openeo-dev.vito.be/openeo/1.0/",
-        "eodc": "https://openeo.eodc.eu/v1.0/",
+        "eodc": "https://openeo-dev.eodc.eu/v1.0/",
         "creo": "https://openeo.creo.vgt.vito.be/openeo/1.0",  # internal version of https://openeo.creo.vito.be/openeo/1.0/
         "sentinelhub": "https://w0j9yieg9l.execute-api.eu-central-1.amazonaws.com/testing",  # Sentinel Hub OpenEO by Sinergise
     },
