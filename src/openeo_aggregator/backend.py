7d52345560cfa4a9dde8833ecbcc63a294837db5
Stefaan Lippens <stefaan.lippens@vito.be>
2022-09-19 15:38:55 +0200

    Add standardization note about `provider:backend`
    also see https://github.com/openEOPlatform/architecture-docs/issues/268

--- src/openeo_aggregator/backend.py
+++ src/openeo_aggregator/backend.py
@@ -57,6 +57,7 @@ class _InternalCollectionMetadata:
 
 class AggregatorCollectionCatalog(AbstractCollectionCatalog):
     # STAC property to use in collection "summaries" and user defined backend selection
+    # TODO: is this the proper standard field to use? Also see https://github.com/openEOPlatform/architecture-docs/issues/268
     STAC_PROPERTY_PROVIDER_BACKEND = "provider:backend"
 
     def __init__(self, backends: MultiBackendConnection):
