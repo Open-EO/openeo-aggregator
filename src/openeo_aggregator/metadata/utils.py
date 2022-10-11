69c1811ede7af9b369d5897d35c156b1796beefa
Jeroen Verstraelen <jeroen.verstraelen@vito.be>
2022-10-11 13:56:29 +0200

    issue #76 fix reporting of unexpected types in merge_dict_values

--- src/openeo_aggregator/metadata/utils.py
+++ src/openeo_aggregator/metadata/utils.py
@@ -29,22 +29,20 @@ def merge_dict_values(
     for cid, d in dictionaries:
         if key in d:
             if expected_types is not None:
-                for expected_type in expected_types:
-                    if not isinstance(d[key], expected_type):
-                        caller = inspect.stack()[1]
-                        report(f"[{cid}]: Unexpected type for {key!r}: {type(d[key])!r} instead of {expected_type!r} in {caller.filename}:{caller.lineno}")
+                if not isinstance(d[key], tuple(expected_types)):
+                    caller = inspect.stack()[1]
+                    report(f"[{cid}]: Unexpected type for {key!r}: {type(d[key])!r} instead of {expected_types!r} in {caller.filename}:{caller.lineno}")
             if result is None:
                 result = d[key]
+            elif isinstance(result, list):
+                if d[key] in result:
+                    continue
+                result = list(result + d[key])
+            elif isinstance(result, dict):
+                result = {**result, **d[key]}
+            elif hasattr(result, 'merge'):
+                result = result.merge(d[key])
             else:
-                if isinstance(result, list):
-                    if d[key] in result:
-                        continue
-                    result = list(result + d[key])
-                elif isinstance(result, dict):
-                    result = {**result, **d[key]}
-                elif hasattr(result, 'merge'):
-                    result = result.merge(d[key])
-                else:
-                    caller = inspect.stack()[1]
-                    report(f"[{cid}]: Unhandled merging of {key!r} with {type(result)} and {type(d[key])} in {caller.filename}:{caller.lineno}")
+                caller = inspect.stack()[1]
+                report(f"[{cid}]: Unhandled merging of {key!r} with {type(result)} and {type(d[key])} in {caller.filename}:{caller.lineno}")
     return result
