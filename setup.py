16c637801ee50c3522589d7b6ee1799ccc847ad5
Stefaan Lippens <stefaan.lippens@vito.be>
2023-05-16 16:04:15 +0200

    setup.py: bump openeo dep to official release

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     install_requires=[
         "requests",
         "attrs",
-        "openeo>=0.17.0.a2.dev",
+        "openeo>=0.17.0",
         "openeo_driver>=0.40.0a1.dev",
         "flask~=2.0",
         "gunicorn~=20.0",
