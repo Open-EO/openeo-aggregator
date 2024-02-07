306e54346981606b8781e645db7a2ca7a3ed1f31
Stefaan Lippens <stefaan.lippens@vito.be>
2024-02-07 09:58:00 +0100

    Issue #117 more descriptive id for default AggregatorBackendConfig

--- conf/backend_config.py
+++ conf/backend_config.py
@@ -1,8 +1,8 @@
 from openeo_aggregator.config import AggregatorBackendConfig
 
 config = AggregatorBackendConfig(
-    # TODO: eliminate hardcoded openEO Platform references.
-    id="aggregator",
+    # TODO: eliminate hardcoded openEO Platform references. https://github.com/Open-EO/openeo-aggregator/issues/117
+    id="openeo-platform-aggregator",
     capabilities_title="openEO Platform",
     capabilities_description="openEO Platform, provided through openEO Aggregator Driver",
     enable_basic_auth=False,
